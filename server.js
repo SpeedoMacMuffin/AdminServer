bf83db8ccd6c94bf717451b817cab9cff22b936e
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-17 09:33:03 +0100

    added bcrypt and password-routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+const db = require("./dbConfig");
 const server = require("./serverConfig");
 const PORT = server.PORT;
 
@@ -10,12 +12,79 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  let id = req.body;
-  console.log(id);
+const hashs = db.collection("hashs");
+const crypto = db.collection("cryptos");
+
+const passCheck = async (req, res, next) => {
+  const dbCheck = await hashs.findOne({ name: "sudo" });
+  if (dbCheck == null) {
+    await bcrypt.hash("ChangeMe", 10, (err, hash) => {
+      if (err) {
+        console.log(err);
+      } else {
+        hashs.insertOne({ name: "sudo", hash: hash, required: true });
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+};
+
+app.get("/auth", passCheck, async (req, res) => {
+  const pass = await hashs.findOne({ name: "sudo" });
+  console.log(pass);
   res.json({
-    message: "wooop to you too!",
+    auth: pass.required,
   });
 });
 
+app.post("/auth", passCheck, async (req, res) => {
+  const { password } = req.body;
+  const pass = await hashs.findOne({ name: "sudo" });
+  bcrypt.compare(password, pass.hash, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json({
+        match: result,
+      });
+    }
+  });
+});
+
+const compare = async (req, res, next) => {
+  const { password } = req.body;
+  const pass = await hashs.findOne({ name: "sudo" });
+  await bcrypt.compare(password, pass.hash, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      if (!result) {
+        return res.json({
+          message: "incorrect Password!",
+          changed: result,
+        });
+      } else {
+        next();
+      }
+    }
+  });
+};
+app.put("/auth", passCheck, compare, async (req, res) => {
+  const { password, newPassword } = req.body;
+
+  bcrypt.hash(newPassword, 10, (err, hash) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const filter = { name: "sudo" };
+      hashs.updateOne(filter, { $set: { hash: hash } });
+      res.json({
+        message: "Password updated!",
+        changed: true,
+      });
+    }
+  });
+});
 app.listen(PORT, console.log(`DeadNode-AdminServer connected on port ${PORT}`));
